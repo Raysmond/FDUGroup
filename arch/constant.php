1a066db5572bc766667dce1d1a9a41e12a51a7aa
Raysmond <jiankunlei@126.com>
2013-10-10 23:54:33 +0800

    Add database model and SQL scripts.

---
+++ arch/constant.php
@@ -0,0 +1,23 @@
+ <?php
+ /**
+  * @file
+  * Constants definition
+  *
+  * @author: Raysmond
+  *
+  */
+ class Constant{
+ 
+	 const db_name = 'fdugroup';
+	 
+	 const db_user = 'fdugroup';
+	 
+	 const db_password = 'fdugroup';
+	 
+	 const db_port = '3306';
+	 
+	 const db_charset = 'utf8';
+ }
+ 
+ echo Constant::db_name;
+ ?>
