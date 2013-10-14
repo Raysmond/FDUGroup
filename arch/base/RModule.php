ad9c10fca93f5604fa4189ecad67fff6b47dac18
Raysmond <jiankunlei@126.com>
2013-10-15 00:49:57 +0800

    modules access support

--- arch/base/RModule.php
+++ arch/base/RModule.php
@@ -4,7 +4,8 @@
  * @author: Raysmond
  */
 
-class RModule {
+class RModule
+{
     // The name of the module
     public $name;
 
@@ -14,11 +15,19 @@ class RModule {
     // The path of the module directory
     private $_path;
 
-    public function __construct($params=array()){
-        if(isset($params['id']))
+    // The module shall appear in what pages
+    // For example:
+    // array('site/about','user/*')
+    // * cannot be the first character
+    // <front> for the front page
+    public $access = array();
+
+    public function __construct($params = array())
+    {
+        if (isset($params['id']))
             $this->setId($params['id']);
 
-        if(isset($params['name']))
+        if (isset($params['name']))
             $this->setName($params['name']);
     }
 
@@ -27,7 +36,8 @@ class RModule {
      * You should override the method if you wanna do some initial work before
      * running the module
      */
-    public function init(){
+    public function init()
+    {
 
     }
 
@@ -35,9 +45,13 @@ class RModule {
      * Run the module
      * This is the place where the module output it's content
      */
-    public function run(){
-        $content = $this->module_content();
-        echo $content;
+    public function run()
+    {
+        if ($this->canAccess()) {
+            $content = $this->module_content();
+            echo $content;
+        } else
+            return false;
     }
 
     /**
@@ -45,31 +59,75 @@ class RModule {
      * This is the right place where you return the output content of the module
      * @return string
      */
-    public function module_content(){
+    public function module_content()
+    {
         return '';
     }
 
-    public function getModuleDir(){
-        if(!isset($this->_path)){
-            $this->_path = Rays::app()->modulePath."/".$this->getId();
+    public function getModuleDir()
+    {
+        if (!isset($this->_path)) {
+            $this->_path = Rays::app()->modulePath . "/" . $this->getId();
         }
         return $this->_path;
     }
 
-    public function setId($id){
+    public function setId($id)
+    {
         $this->_id = $id;
     }
 
-    public function getId(){
+    public function getId()
+    {
         return $this->_id;
     }
 
-    public function setName($name){
+    public function setName($name)
+    {
         $this->name = $name;
     }
 
-    public function getName(){
+    public function getName()
+    {
         return $this->name;
+    }
+
+    /**
+     * Whether the current page can access the module
+     */
+    public function canAccess()
+    {
+        if (!is_array($this->access))
+            return false;
+        // like : user/view/1
+        $currentUrl = Rays::app()->getHttpRequest()->getRequestUriInfo();
+        if($currentUrl=='') // front page
+            $currentUrl = '<front>';
+        foreach ($this->access as $url) {
+            if ($url == $currentUrl)
+                return true;
+            else {
+                if (($pos = strpos($url, '*')) > 0) {
+                    $arr = explode('*', $url);
+                    foreach ($arr as $part) {
+                        if ($part == '') continue;
+                        if (($apartPos = strpos($currentUrl, $part)) == false) {
+                            $sub = substr($currentUrl, 0, strlen($part));
+                            if ($sub != $part) {
+                                return false;
+                            } else {
+                                $currentUrl = substr($currentUrl, strlen($part));
+                            }
+                        } else {
+                            $currentUrl = substr($currentUrl, $apartPos + strlen($part));
+                        }
+                    }
+                } else {
+                    //
+                }
+            }
+        }
+        return true;
     }
 
 }
