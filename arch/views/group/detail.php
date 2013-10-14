45a517c7a8e45e1f4e13d528dd87f3b4eb84f6d1
junshiguo <junshiguo@126.com>
2013-10-15 01:52:44 +0800

    detail changes to group related pages; add edit page which is to be done

--- arch/views/group/detail.php
+++ arch/views/group/detail.php
@@ -4,13 +4,34 @@
  * @author: Raysmond
  */
 ?>
+<h2>
+    <?php
+    echo $group->name.'&nbsp&nbsp&nbsp';
+    if(Rays::app()->isUserLogin()){
+        $g_u = new GroupUser();
+        $g_u->userId = Rays::app()->getLoginUser()->id;
+        $g_u->groupId = $group->id;
+        $g_u = $g_u->find();
+        if(count($g_u)==0){
+            echo RHtmlHelper::linkAction('group','+ Join the group','join',$group->id,array('class'=>'btn btn-xs btn-info'));
+        }else{
+            if($group->creator == Rays::app()->getLoginUser()->id){
+                echo RHtmlHelper::linkAction('group','Manager: Edit group','edit',$group->id,array('class'=>'btn btn-xs btn-info'));
+            }else
+                echo RHtmlHelper::linkAction('group','- Exit group','exit',$group->id,array('class'=>'btn btn-xs btn-info'));
+        }
+    }else{
+        echo RHtmlHelper::linkAction('group','+ Join the group','join',$group->id,array('class'=>'btn btn-xs btn-info'));
+    }
+    //echo RHtmlHelper::linkAction('group','Join group','detail',null,array('class'=>'btn btn-success'));
+    ?>
+</h2>
 <div class="jumbotron">
-<h2><?php echo $group->name;?></h2>
-<p>
-Group creator: <?php echo RHtmlHelper::linkAction('user',$group->groupCreator->name,'view',$group->creator) ?> <br/>
-Group category: <?php echo RHtmlHelper::linkAction('category',$group->category->name,'view',$group->category->id); ?> <br/>
-Group introduction: <?php echo $group->intro; ?> <br/>
-Group members: <?php echo $group->memberCount; ?> <br/>
-Group created time: <?php echo $group->createdTime; ?> <br/>
-</p>
+<p><h4>
+    create time:<?php echo $group->createdTime; ?> &nbsp &nbsp
+    created by: <?php echo RHtmlHelper::linkAction('user',$group->groupCreator->name,'view',$group->creator) ?> &nbsp &nbsp
+    group type: <?php echo RHtmlHelper::linkAction('category',$group->category->name,'view',$group->category->id); ?> <br/>
+    Group members: <?php echo $group->memberCount; ?> <br/>
+    <br/>Group introduction: <?php echo $group->intro; ?> <br/>
+</h4></p>
 </div>
