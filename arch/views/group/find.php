6db825ee62476c81eff23fa2d1140d974e58003e
Raysmond <jiankunlei@126.com>
2013-10-14 23:45:05 +0800

    find group layout

--- arch/views/group/find.php
+++ arch/views/group/find.php
@@ -23,20 +23,20 @@
             else
                 echo '</div><div class="row">';
         }
-        echo "<div class='panel panel-default'>";
+        echo '<div class="col-6 col-sm-6 col-lg-4" style="height: 190px;">';
+        echo "<div class='panel panel-default' style='height: 170px;'>";
         echo "<div class='panel-heading'>";
-        echo RFormHelper::label($group->name);
-        echo '&nbsp;&nbsp;';
-        echo RHtmlHelper::linkAction('group','Join','join',$group->id);
-        echo '&nbsp;&nbsp;';
-        echo RHtmlHelper::linkAction('group','View details','detail',$group->id);
+        echo RHtmlHelper::linkAction('group',$group->name,'detail',$group->id);
         echo "</div>";
         echo "<div class='panel-body'>";
-        foreach($group->columns as $col=>$coldb){
-            if(isset($group->$col))
-                echo RFormHelper::label($group->$col)."      ";
+        echo $group->memberCount." members";
+        if(strlen($group->intro)>100){
+            echo '<p>'.substr($group->intro,0,100).'...</p>';
         }
-        echo "</div></div>";
+        else echo '<p>'.$group->intro.'</p>';
+        echo RHtmlHelper::linkAction('group','Join the group','join',$group->id,
+            array('class'=>'btn btn-xs btn-info','style'=>'position:absolute;top:135px;'));
+        echo "</div></div></div>";
         $flag = ($flag+1)%3;
     }
     echo "</div>";
