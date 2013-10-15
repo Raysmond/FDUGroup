9430d6c3f132799d079b2c772224d93d6cd61750
Raysmond <jiankunlei@126.com>
2013-10-16 00:29:52 +0800

    fix bugs

--- arch/base/RHttpRequest.php
+++ arch/base/RHttpRequest.php
@@ -124,8 +124,8 @@ class RHttpRequest
         if (($pos = strpos($uri, "?q=")) > 0)
             return substr($uri, $pos + 3);
         else {
-            $uri =  substr($uri, strlen(Rays::app()->getBaseUrl()) - strlen("http://" . $this->getServerName()) + 1);
-            return str_replace("index.php","",$uri);
+            $uri = substr($uri, strlen(Rays::app()->getBaseUrl()) - strlen("http://" . $this->getServerName()) + 1);
+            return str_replace("index.php", "", $uri);
         }
     }
 
@@ -135,26 +135,33 @@ class RHttpRequest
      * @param string $url default the front page
      * @return bool
      */
-    public function urlMatch($urlRules=array(),$url=''){
-        if (!is_array($urlRules)){
+
+
+    public function urlMatch($urlRules = array(), $url = '')
+    {
+        if (!is_array($urlRules)) {
             $urlRules = array($urlRules);
         }
         // like : user/view/1
-        $currentUrl = $url!=''?$url : $this->getRequestUriInfo();
-        if($currentUrl=='')   // front page
+        $currentUrl = $url != '' ? $url : $this->getRequestUriInfo();
+        if ($currentUrl == '') // front page
         $currentUrl = '<front>';
+        //print_r($urlRules);
         foreach ($urlRules as $url) {
             if ($url == $currentUrl)
                 return true;
             else {
                 if (($pos = strpos($url, '*')) > 0) {
                     $arr = explode('*', $url);
+                    $match = true;
                     foreach ($arr as $part) {
                         if ($part == '') continue;
                         if (($apartPos = strpos($currentUrl, $part)) == false) {
                             $sub = substr($currentUrl, 0, strlen($part));
                             if ($sub != $part) {
-                                return false;
+                                $match = false;
+                                break;
+                                //return false;
                             } else {
                                 $currentUrl = substr($currentUrl, strlen($part));
                             }
@@ -162,7 +169,7 @@ class RHttpRequest
                             $currentUrl = substr($currentUrl, $apartPos + strlen($part));
                         }
                     }
-                    return true;
+                    if ($match) return true;
                 } else {
                     //
                 }
