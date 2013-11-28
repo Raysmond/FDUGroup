f9aa4cb4bcf2e2bd185e6289543a55eb2c8543fe
RenchuSong <songrenchu@sina.com>
2013-11-29 00:31:37 +0800

    Liked Topis in Public Profile

--- arch/views/user/view.php
+++ arch/views/user/view.php
@@ -130,6 +130,27 @@
             </table>
         <?php
             }
+        } else
+        if ($part == 'likes') {
+            if (!count($likeTopics)) {
+                echo "<p>This guy has not posted any topics!</p>";
+            } else {
+                ?>
+                <table class="table table-hover table-condensed">
+                    <thead><tr><th>Title</th><th>Replies</th><th>Time</th><th>Last comment</th></tr></thead>
+                    <tbody><?php
+
+                    foreach ($likeTopics as $topic) {
+                        ?><tr><td><b><?=RHtmlHelper::linkAction('post', $topic->title, 'view', $topic->id)?></b></td>
+                        <td><?=$topic->commentCount?></td>
+                        <td><?=$topic->createdTime?></td>
+                        <td><?=$topic->lastCommentTime?></td></tr><?php
+                    }
+
+                    ?></tbody>
+                </table>
+            <?php
+            }
         }
 
         ?>
