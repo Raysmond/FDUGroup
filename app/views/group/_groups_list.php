0081dc416e23c72d3af896708ef329cf211e4e9b
Raysmond <jiankunlei@126.com>
2013-12-04 22:47:19 +0800

    masonry group lists

--- app/views/group/_groups_list.php
+++ app/views/group/_groups_list.php
@@ -2,41 +2,41 @@
 foreach ($groups as $group) {
     ?>
     <div class="item col-lg-4">
-        <?php
-        if ($group->picture) {
-            ?>
-            <div class="group-picture"> <?php
-                $picture = RImageHelper::styleSrc($group->picture, Group::getPicOptions());
-                echo RHtmlHelper::showImage($picture, $group->name);
-                ?> </div> <?php
-        }
-        ?>
-        <div class='item-heading'>
-            <?php
-            echo RHtmlHelper::linkAction('group', $group->name, 'detail', $group->id);
-            ?> </div> <?php
 
-        ?>
+        <!-- Group picture -->
+        <?php if ($group->picture) { ?>
+            <div class="group-picture">
+                <?= RHtmlHelper::showImage(RImageHelper::styleSrc($group->picture, Group::getPicOptions()), $group->name); ?>
+            </div>
+        <?php } ?>
+
+        <!-- Group name -->
+        <div class='item-heading'>
+            <?= RHtmlHelper::linkAction('group', $group->name, 'detail', $group->id); ?>
+        </div>
+
+        <!-- Group intro -->
         <div class="item-body">
             <?php
             $group->intro = strip_tags(RHtmlHelper::decode($group->intro));
             if (mb_strlen($group->intro) > 100) {
                 echo '<p>' . mb_substr($group->intro, 0, rand(60, 150), 'UTF-8') . '...</p>';
-            } else echo '<p>' . $group->intro . '</p>';
-
+            } else {
+                echo '<p>' . $group->intro . '</p>';
+            }
             ?>
 
         </div>
 
+        <!-- Group actions -->
         <div class="actions">
+
             <div class="action">
-                <span class="glyphicon glyphicon-user"></span>
-                <?=$group->memberCount?>
+                <span class="glyphicon glyphicon-user"></span> <?= $group->memberCount ?>
             </div>
+
             <div class="action action-like">
-                <?php
-                $this->module('rating_plus', array('id' => 'rating_plus', 'entityType' => Group::ENTITY_TYPE, 'entityId' => $group->id));
-                ?>
+                <?php $this->module('rating_plus', array('id' => 'rating_plus', 'entityType' => Group::ENTITY_TYPE, 'entityId' => $group->id)); ?>
             </div>
 
             <?php
@@ -46,9 +46,9 @@ foreach ($groups as $group) {
                 $g_u->groupId = $group->id;
                 if (count($g_u->find()) == 0) {
                     ?>
-                <div class="action">
-                    <a href="#"><span class="glyphicon glyphicon-plus"></span></a>
-                </div>
+                    <div class="action">
+                        <a href="#"><span class="glyphicon glyphicon-plus"></span></a>
+                    </div>
                 <?php
                 }
             }
@@ -56,13 +56,14 @@ foreach ($groups as $group) {
             if (isset($exitGroup)) {
                 ?>
                 <div class="action">
-                    <a href="<?=RHtmlHelper::siteUrl("group/exit/".$group->id)?>" onclick='javascript:confirmExit(<?=$group->id?>)'><span class="glyphicon glyphicon-remove"></span></a>
+                    <a href="<?= RHtmlHelper::siteUrl("group/exit/" . $group->id) ?>"
+                       onclick='javascript:confirmExit(<?= $group->id ?>)'><span
+                            class="glyphicon glyphicon-remove"></span></a>
                 </div>
-                <?php
+            <?php
             }
             ?>
         </div>
-
-        <?php
-        ?> </div> <?php
+    </div>
+<?php
 }
