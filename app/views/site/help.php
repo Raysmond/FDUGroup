db2aea6776b057587a6e4fee9ae9d91c28aab41b
Raysmond <jiankunlei@126.com>
2013-12-06 22:10:15 +0800

    site help

--- app/views/site/help.php
+++ app/views/site/help.php
@@ -1,162 +1,165 @@
-<div class="page-header">
-    <h1>FDUGroup Help</h1>
-</div>
+<div class="panel panel-wrapper">
 
-<div id="register_and_login" class="page-header">
     <div class="page-header">
-        <h2>Register and login</h2>
+        <h1>FDUGroup Help</h1>
     </div>
-    <img src="../files/images/site/register_button.jpg" class="img-responsive" alt="Responsive image">
-    <p>登录网站后，单击网页右上角“Login”按钮（上图右上角蓝色标记部分），即可登录。登陆界面下方左图所示。<br>
-        若尚未申请账号，请单击网页右上角“Register”按钮（上图右上角红色标记部分），并完成下方右图所示的信息，即可完成注册！</p><br>
-    <img src="../files/images/site/Login.jpg" class="img-responsive col-xs-6 left" alt="Responsive image">
-    <img src="../files/images/site/register.jpg" class="img-responsive col-xs-6 right" alt="Responsive image">
-</div>
 
-<div id="find_group">
-    <br><br><br>
-    <div class="page-header">
-        <h2>Find Group</h2>
+    <div id="register_and_login" class="page-header">
+        <div class="page-header">
+            <h2>Register and login</h2>
+        </div>
+        <img src="../files/images/site/register_button.jpg" class="img-responsive" alt="Responsive image">
+        <p>登录网站后，单击网页右上角“Login”按钮（上图右上角蓝色标记部分），即可登录。登陆界面下方左图所示。<br>
+            若尚未申请账号，请单击网页右上角“Register”按钮（上图右上角红色标记部分），并完成下方右图所示的信息，即可完成注册！</p><br>
+        <img src="../files/images/site/Login.jpg" class="img-responsive col-xs-6 left" alt="Responsive image">
+        <img src="../files/images/site/register.jpg" class="img-responsive col-xs-6 right" alt="Responsive image">
     </div>
-    <img src="../files/images/site/group.jpg" class="img-responsive" alt="Responsive image">
-    <p>您随时可以通过单击网页顶端的“Find Group”按钮（具体位置见上图）寻找感兴趣的小组。</p>
-    <br>
-    <p>在这里，您可以选择自己感兴趣的小组，只需单击该小组内的“join the group”，即可申请加入该小组，见下图红色标记部分。待小组管理员同意后，即可加入该小组。
-        您也可以通过搜索框直接搜索您感兴趣的小组，见下图蓝色标记内容。</p>
-    <br>
-    <p>PS：加入小组的前提是您已经登录到网站，若无账号，请先进行注册。</p>
-    <img src="../files/images/site/find_group.jpg" class="img-responsive img-thumbnail" alt="Responsive image">
-</div>
 
-<div id="my_group">
-    <br><br><br>
-    <div class="page-header">
-        <h2>My Group</h2>
+    <div id="find_group">
+        <br><br><br>
+        <div class="page-header">
+            <h2>Find Group</h2>
+        </div>
+        <img src="../files/images/site/group.jpg" class="img-responsive" alt="Responsive image">
+        <p>您随时可以通过单击网页顶端的“Find Group”按钮（具体位置见上图）寻找感兴趣的小组。</p>
+        <br>
+        <p>在这里，您可以选择自己感兴趣的小组，只需单击该小组内的“join the group”，即可申请加入该小组，见下图红色标记部分。待小组管理员同意后，即可加入该小组。
+            您也可以通过搜索框直接搜索您感兴趣的小组，见下图蓝色标记内容。</p>
+        <br>
+        <p>PS：加入小组的前提是您已经登录到网站，若无账号，请先进行注册。</p>
+        <img src="../files/images/site/find_group.jpg" class="img-responsive img-thumbnail" alt="Responsive image">
     </div>
-    <img src="../files/images/site/group.jpg" class="img-responsive" alt="Responsive image">
-    <p>您可以通过单击网页顶端的“My Group”按钮（具体位置见上图）对已加入的小组进行管理。</p>
-    <p>你可以通过“View detail”来查看该小组的详细信息，也可以通过“Exit group”退出该小组。（如图黑色标记部分）</p>
-    <p>PS：管理小组的前提是您已经登录到网站，若无账号，请先进行注册。</p>
-    <img src="../files/images/site/mygroup.jpg" class="img-responsive img-thumbnail" alt="Responsive image">
-    <p>你也可以创建自己的小组，只需单击上图绿色按钮即可。（如图红色标记内容）创建小组页面如下图所示:</p>
-    <p>为完成小组创建，你需要为你的小组添加：名字、小组类型、以及关于小组必要的介绍等基本内容。你也可以向小组上传内容。当内容填写完毕后，单击
-        “Build now”按钮，即可完成一个话题小组的创建！
-    </p>
-    <img src="../files/images/site/buildgroup.jpg" class="img-responsive img-thumbnail" alt="Responsive image">
-    <p>现在，您可以在find group界面中寻找您刚刚创建的小组了!</p>
-</div>
 
-<div id="topic">
-    <br><br><br>
-    <div class="page-header">
-        <h2>Topic</h2>
+    <div id="my_group">
+        <br><br><br>
+        <div class="page-header">
+            <h2>My Group</h2>
+        </div>
+        <img src="../files/images/site/group.jpg" class="img-responsive" alt="Responsive image">
+        <p>您可以通过单击网页顶端的“My Group”按钮（具体位置见上图）对已加入的小组进行管理。</p>
+        <p>你可以通过“View detail”来查看该小组的详细信息，也可以通过“Exit group”退出该小组。（如图黑色标记部分）</p>
+        <p>PS：管理小组的前提是您已经登录到网站，若无账号，请先进行注册。</p>
+        <img src="../files/images/site/mygroup.jpg" class="img-responsive img-thumbnail" alt="Responsive image">
+        <p>你也可以创建自己的小组，只需单击上图绿色按钮即可。（如图红色标记内容）创建小组页面如下图所示:</p>
+        <p>为完成小组创建，你需要为你的小组添加：名字、小组类型、以及关于小组必要的介绍等基本内容。你也可以向小组上传内容。当内容填写完毕后，单击
+            “Build now”按钮，即可完成一个话题小组的创建！
+        </p>
+        <img src="../files/images/site/buildgroup.jpg" class="img-responsive img-thumbnail" alt="Responsive image">
+        <p>现在，您可以在find group界面中寻找您刚刚创建的小组了!</p>
     </div>
-    <p>进入您感兴趣的小组后，即可查看和发表与该小组话题相关的topic。
-        点击任一主题，即可查看该主题中的内容。也可以通过“more topic”按钮查看所有主题。</p>
-    <img src="../files/images/site/topic.jpg" class="img-responsive" alt="Responsive image">
-    <p>当然，你也可以通过绿色按钮“Add new post”，在小组内发表话题！如下图所示：</p>
-    <img src="../files/images/site/newtopic.jpg" class="img-responsive" alt="Responsive image">
-    <p>在上图中输入你要发表的话题名称及内容，点击post，即可发表话题。我们提供了非常友好的编辑界面，
-        有多种字体和格式供你选择，你甚至可以向其中插入表情和图片。</p>
-    <p>你还可以对别人的话题进行评论，以及与他人进行互动。回复他人的评论等。还可以对该话题进行点赞功能！</p>
-    <img src="../files/images/site/replytopic.jpg" class="img-responsive img-thumbnail" alt="Responsive image">
-    <img src="../files/images/site/reply.jpg" class="img-responsive" alt="Responsive image">
-</div>
 
-<div id="homepage">
-    <br><br><br>
-    <div class="page-header">
-        <h2>Home Page</h2>
+    <div id="topic">
+        <br><br><br>
+        <div class="page-header">
+            <h2>Topic</h2>
+        </div>
+        <p>进入您感兴趣的小组后，即可查看和发表与该小组话题相关的topic。
+            点击任一主题，即可查看该主题中的内容。也可以通过“more topic”按钮查看所有主题。</p>
+        <img src="../files/images/site/topic.jpg" class="img-responsive" alt="Responsive image">
+        <p>当然，你也可以通过绿色按钮“Add new post”，在小组内发表话题！如下图所示：</p>
+        <img src="../files/images/site/newtopic.jpg" class="img-responsive" alt="Responsive image">
+        <p>在上图中输入你要发表的话题名称及内容，点击post，即可发表话题。我们提供了非常友好的编辑界面，
+            有多种字体和格式供你选择，你甚至可以向其中插入表情和图片。</p>
+        <p>你还可以对别人的话题进行评论，以及与他人进行互动。回复他人的评论等。还可以对该话题进行点赞功能！</p>
+        <img src="../files/images/site/replytopic.jpg" class="img-responsive img-thumbnail" alt="Responsive image">
+        <img src="../files/images/site/reply.jpg" class="img-responsive" alt="Responsive image">
     </div>
-    <p>登陆后，单击网页右上角个人账号，点击Home Page，即可进入该界面。</p>
-    <p>你可以在这里查看自己发表过的话题、管理个人信息、申请VIP、以及查看收到的Message等。网页右上方将显示你已经添加过的好友。</p>
-    <img src="../files/images/site/homepage.jpg" class="img-responsive" alt="Responsive image">
-</div>
 
-<div id="postmanagement">
-    <br><br><br>
-    <div class="page-header">
-        <h2>Post Management</h2>
+    <div id="homepage">
+        <br><br><br>
+        <div class="page-header">
+            <h2>Home Page</h2>
+        </div>
+        <p>登陆后，单击网页右上角个人账号，点击Home Page，即可进入该界面。</p>
+        <p>你可以在这里查看自己发表过的话题、管理个人信息、申请VIP、以及查看收到的Message等。网页右上方将显示你已经添加过的好友。</p>
+        <img src="../files/images/site/homepage.jpg" class="img-responsive" alt="Responsive image">
     </div>
-    <p>进入home page（详见上）页面后，单击My Post，即可对自己发表的话题和评论进行管理。如回复和删除操作。</p>
-    <p>你也可在点击“Lastst post”查看最新的评论。</p>
-    <img src="../files/images/site/post.jpg" class="img-responsive" alt="Responsive image">
-</div>
 
-<div id="profile">
-    <br><br><br>
-    <div class="page-header">
-        <h2>User Profile</h2>
+    <div id="postmanagement">
+        <br><br><br>
+        <div class="page-header">
+            <h2>Post Management</h2>
+        </div>
+        <p>进入home page（详见上）页面后，单击My Post，即可对自己发表的话题和评论进行管理。如回复和删除操作。</p>
+        <p>你也可在点击“Lastst post”查看最新的评论。</p>
+        <img src="../files/images/site/post.jpg" class="img-responsive" alt="Responsive image">
     </div>
-    <p>单击“Profile”，即可对查看个人信息。若要对个人信息进行修改，单击Edit Profile即可。 <p>
-    <p>另外，你也可以在该页面提出VIP申请。拥有VIP特权的用户将会享受到更多的特权。</p>
-    <img src="../files/images/site/profile.jpg" class="img-responsive" alt="Responsive image">
-</div>
 
-
-<div id="message">
-    <br><br><br>
-    <div class="page-header">
-        <h2>Message Management</h2>
+    <div id="profile">
+        <br><br><br>
+        <div class="page-header">
+            <h2>User Profile</h2>
+        </div>
+        <p>单击“Profile”，即可对查看个人信息。若要对个人信息进行修改，单击Edit Profile即可。 <p>
+        <p>另外，你也可以在该页面提出VIP申请。拥有VIP特权的用户将会享受到更多的特权。</p>
+        <img src="../files/images/site/profile.jpg" class="img-responsive" alt="Responsive image">
     </div>
-    <img src="../files/images/site/message.jpg" class="img-responsive" alt="Responsive image">
-    <p>在个人主页上，单击“Messages”即可对所有邮件进行管理，在这里，你可以发送、接受查看邮件，并对邮件进行操作。</p>
-    <p>你也可以通过单击个人账号下的Messages框进入该界面。</p>
-    <img src="../files/images/site/message_user.jpg" class="img-responsive" alt="Responsive image">
-    <p>PS.Message右侧所示数字表示未读邮件数目。</p>
-</div>
 
-<div id="personalpage">
-    <br><br><br>
-    <div class="page-header">
-        <h2>Personal Page</h2>
+
+    <div id="message">
+        <br><br><br>
+        <div class="page-header">
+            <h2>Message Management</h2>
+        </div>
+        <img src="../files/images/site/message.jpg" class="img-responsive" alt="Responsive image">
+        <p>在个人主页上，单击“Messages”即可对所有邮件进行管理，在这里，你可以发送、接受查看邮件，并对邮件进行操作。</p>
+        <p>你也可以通过单击个人账号下的Messages框进入该界面。</p>
+        <img src="../files/images/site/message_user.jpg" class="img-responsive" alt="Responsive image">
+        <p>PS.Message右侧所示数字表示未读邮件数目。</p>
     </div>
-    <img src="../files/images/site/personal.jpg" class="img-responsive" alt="Responsive image">
-    <p>通过单击个人账号下的Personal Page框即可进入该界面。</p>
-    <p>你可以在这里查看包括：已加入小组、post、个人信息、Favorite等信息。</p>
-</div>
 
-
-<div id="friend">
-    <br><br><br>
-    <div class="page-header">
-        <h2>Friend</h2>
+    <div id="personalpage">
+        <br><br><br>
+        <div class="page-header">
+            <h2>Personal Page</h2>
+        </div>
+        <img src="../files/images/site/personal.jpg" class="img-responsive" alt="Responsive image">
+        <p>通过单击个人账号下的Personal Page框即可进入该界面。</p>
+        <p>你可以在这里查看包括：已加入小组、post、个人信息、Favorite等信息。</p>
     </div>
-    <p>通过单击某个个人账号下并向他提出好友申请。通过申请后，就可以查看他的个人信息以及他的已加入小组、post、Favorite等信息。</p>
-</div>
 
-<div id="vip">
-    <br><br><br>
-    <div class="page-header">
-        <h2>VIP Apply</h2>
-    </div>
-    <p>你可以在该页面提出VIP申请。拥有VIP特权的用户将会享受到更多的特权。</p>
-    <img src="../files/images/site/profile.jpg" class="img-responsive" alt="Responsive image">
-</div>
 
-<div id="AdSense">
-    <br><br><br>
-    <div class="page-header">
-        <h2>AdSense</h2>
+    <div id="friend">
+        <br><br><br>
+        <div class="page-header">
+            <h2>Friend</h2>
+        </div>
+        <p>通过单击某个个人账号下并向他提出好友申请。通过申请后，就可以查看他的个人信息以及他的已加入小组、post、Favorite等信息。</p>
     </div>
-    <p>你可以在网站上投放广告，推广您的产品.请联系我们！</p>
-    <img src="../files/images/site/ads.jpg" class="img-responsive" alt="Responsive image">
-</div>
 
-<div id="publish">
-    <br><br><br>
-    <div class="page-header">
-        <h2>About Publish</h2>
+    <div id="vip">
+        <br><br><br>
+        <div class="page-header">
+            <h2>VIP Apply</h2>
+        </div>
+        <p>你可以在该页面提出VIP申请。拥有VIP特权的用户将会享受到更多的特权。</p>
+        <img src="../files/images/site/profile.jpg" class="img-responsive" alt="Responsive image">
     </div>
-    <p>依据相关法律规定，网站禁止传播以下内容：</p>
-    <p>(一)反对宪法所确定的基本原则的；</p>
-    <p>(二)危害国家安全，泄露国家秘密，颠覆国家政权，破坏国家统一的；</p>
-    <p> (三)损害国家荣誉和利益的；</p>
-    <p> (四)煽动民族仇恨、民族歧视，破坏民族团结的；</p>
-    <p> (五)破坏国家宗教政策，宣扬邪教和封建迷信的；</p>
-    <p> (六)散布谣言，扰乱社会秩序，破坏社会稳定的；</p>
-    <p> (七)散布淫秽、色情、赌博、暴力、凶杀、恐怖或者教唆犯罪的；</p>
-    <p> (八)侮辱或者诽谤他人，侵害他人合法权益的；</p>
-    <p> (九)含有法律、行政法规禁止的其它内容的。</p>
-    <p> 不良信息是指违背社会主义精神文明建设要求、违背中华民族优良文化传统与习惯以及其它违背社会公德的各类信息，包括文字、图片、音视频等等。</p>
+
+    <div id="AdSense">
+        <br><br><br>
+        <div class="page-header">
+            <h2>AdSense</h2>
+        </div>
+        <p>你可以在网站上投放广告，推广您的产品.请联系我们！</p>
+        <img src="../files/images/site/ads.jpg" class="img-responsive" alt="Responsive image">
+    </div>
+
+    <div id="publish">
+        <br><br><br>
+        <div class="page-header">
+            <h2>About Publish</h2>
+        </div>
+        <p>依据相关法律规定，网站禁止传播以下内容：</p>
+        <p>(一)反对宪法所确定的基本原则的；</p>
+        <p>(二)危害国家安全，泄露国家秘密，颠覆国家政权，破坏国家统一的；</p>
+        <p> (三)损害国家荣誉和利益的；</p>
+        <p> (四)煽动民族仇恨、民族歧视，破坏民族团结的；</p>
+        <p> (五)破坏国家宗教政策，宣扬邪教和封建迷信的；</p>
+        <p> (六)散布谣言，扰乱社会秩序，破坏社会稳定的；</p>
+        <p> (七)散布淫秽、色情、赌博、暴力、凶杀、恐怖或者教唆犯罪的；</p>
+        <p> (八)侮辱或者诽谤他人，侵害他人合法权益的；</p>
+        <p> (九)含有法律、行政法规禁止的其它内容的。</p>
+        <p> 不良信息是指违背社会主义精神文明建设要求、违背中华民族优良文化传统与习惯以及其它违背社会公德的各类信息，包括文字、图片、音视频等等。</p>
+    </div>
 </div>
