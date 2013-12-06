5e2b91c63d4b1afc786e9d3f7c049590fca77933
Raysmond <jiankunlei@126.com>
2013-12-06 19:17:50 +0800

    user panel module

--- app/views/layout/user.php
+++ app/views/layout/user.php
@@ -31,67 +31,8 @@ $baseUrl = Rays::app()->getBaseUrl();
 ?>
 
 <div id="main-wrapper" class="container">
-    <div class="row user-panel">
-        <?php $user = Rays::app()->getLoginUser(); if($user!=null): ?>
-            <div class="row user-profile-tiny">
-                <div class="user-picture">
-                    <?php if(!isset($user->picture)||$user->picture=='') $user->picture=User::$defaults['picture'];
-                    $thumbnail = RImageHelper::styleSrc($user->picture,$user::getPicOptions());
-                    ?>
-                    <a href="<?=RHtmlHelper::siteUrl('user/view/'.$user->id)?>" >
-                        <?php
-                        echo RHtmlHelper::showImage($thumbnail,$user->name, array('class'=>'img-thumbnail','width'=>'120px'))
-                        ?>
-                    </a>
-                </div>
 
-                <div class="user-statistics">
-                    <div class="stat-item">
-                        <div class="number">
-                            <?=$user->countFriends($user->id)?>
-                        </div>
-                        <div class="text">
-                            Friends
-                        </div>
-                    </div>
-                    <div class="stat-item">
-                        <div class="number">
-                            <?=$user->countGroups($user->id)?>
-                        </div>
-                        <div class="text">
-                            Groups
-                        </div>
-                    </div>
-                    <div class="stat-item">
-                        <div class="number">
-                            <?=$user->countPosts($user->id)?>
-                        </div>
-                        <div class="text">
-                            Posts
-                        </div>
-                    </div>
-                </div>
-
-                <div class="user-profile">
-                    <h2>
-                        <?=RHtmlHelper::linkAction('user',$user->name,'view',$user->id)?>&nbsp;
-                        <span class="badge badge-<?=Role::getRoleNameById($user->roleId);?>"><?=Role::getBadgeById($user->roleId);?></span>
-                    </h2>
-                    <div><?=RHtmlHelper::decode($user->intro)?></div>
-                    <div><?=$user->region?>
-                        <?php if ($user->weibo!='') { ?>
-                            <?php if ($user->region!='') { ?>|<?php } ?>
-                            Micro-Blog: <?=RHtmlHelper::link($user->weibo,$user->weibo,$user->weibo)?>
-                        <?php } ?>
-                    </div>
-                </div>
-
-            </div>
-
-        <?php endif; ?>
-    </div>
-    <hr>
-
+    <?php $this->module("user_panel"); ?>
 
     <div class="row row-offcanvas row-offcanvas-right">
         <div class="col-xs-12 col-sm-9">
