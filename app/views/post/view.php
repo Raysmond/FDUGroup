6c2ff40b18db4cb67f0cb337e01bae4654e2723e
Raysmond <jiankunlei@126.com>
2013-12-07 17:22:40 +0800

    post view, group detail

--- app/views/post/view.php
+++ app/views/post/view.php
@@ -1,8 +1,9 @@
-<div class="panel panel-wrapper">
+<div class="panel panel-default post">
+    <div class="panel-heading">
+        <h1 class="panel-title"><?= $topic->title ?></h1>
+    </div>
 
-    <div class="post">
-        <h2><?= $topic->title ?></h2>
-
+    <div class="panel-body">
         <div class="post-meta">
             Post by:
             <?= RHtmlHelper::linkAction('user', $topic->user->name, 'view', $topic->user->id) ?>
@@ -23,73 +24,75 @@
         <div style="width: 100%; text-align: center;">
             <?php $this->module('rating_plus', array('id' => 'rating_plus', 'entityType' => Topic::$entityType, 'entityId' => $topic->id)); ?>
         </div>
-    </div>
-
-    <hr/>
-    <div>
-
-        <h2>Comments</h2>
-        <?php
-        foreach ($commentTree as $commentItem) {
-            ?>
-            <div id="comment-item-<?= $commentItem['root']->id ?>" class="row comment-item">
-                <!-- user picture -->
-                <div class="col-lg-1">
-                    <?= RHtmlHelper::showImage($commentItem['root']->user->picture, $commentItem['root']->user->name, array('width' => '64px;')) ?>
-                </div>
-
-                <!-- comment content -->
-                <div class="col-lg-11">
-                    <div><?= RHtmlHelper::linkAction('user', $commentItem['root']->user->name, 'view', $commentItem['root']->user->id) ?>
-                        &nbsp;&nbsp;<?= $commentItem['root']->createdTime ?></div>
-                    <div><?= RHtmlHelper::decode($commentItem['root']->content) ?></div>
-
-                    <div style="float: right;">
-                        <?= RHtmlHelper::linkAction('post', 'Reply', 'view',
-                            $commentItem['root']->topicId . '?reply=' . $commentItem['root']->id . '#reply',
-                            array('class' => 'btn btn-xs btn-info')) ?>
-                    </div>
-
-                    <div class="comment-reply-list" style="margin-top: 10px;padding-left: 20px;">
-                        <?php
-                        foreach ($commentItem['reply'] as $reply) {
-                            ?>
-                            <div class="comment-reply-item" style="margin-top: 10px;"><?= RHtmlHelper::linkAction('user', $reply->user->name, 'view', $reply->user->id) ?>
-                                &nbsp;&nbsp;<?= $reply->createdTime ?></div>
-                            <div><?= RHtmlHelper::decode($reply->content) ?></div>
-                        <?php
-                        }
-                        ?>
-                    </div>
-
-                </div>
-                <br/><br/>
-            </div>
-            <hr>
-        <?php
-        }
-        ?>
-
 
         <hr/>
-        <div id="reply">
-            <?= RFormHelper::openForm("post/comment/$topic->id", array('id' => 'viewFrom')) ?>
-            <?=
-            RFormHelper::textarea(array(
-                'name' => 'content',
-                'class' => 'form-control',
-                'rows' => '5',
-                'placeholder' => 'Comment content',
-            ), isset($parent) ? '@' . $parent->user->name . ' ' : '')?>
+
+        <div>
+            <h2>Comments</h2>
             <?php
-            if (isset($parent)) {
-                echo RFormHelper::hidden('replyTo', ((int)$parent->pid === 0 ? $parent->id : $parent->pid));
-                echo RFormHelper::hidden('exactReplyTo', $parent->id);
+            foreach ($commentTree as $commentItem) {
+                ?>
+                <div id="comment-item-<?= $commentItem['root']->id ?>" class="row comment-item">
+                    <!-- user picture -->
+                    <div class="col-lg-1">
+                        <?= RHtmlHelper::showImage($commentItem['root']->user->picture, $commentItem['root']->user->name, array('width' => '64px;')) ?>
+                    </div>
+
+                    <!-- comment content -->
+                    <div class="col-lg-11">
+                        <div><?= RHtmlHelper::linkAction('user', $commentItem['root']->user->name, 'view', $commentItem['root']->user->id) ?>
+                            &nbsp;&nbsp;<?= $commentItem['root']->createdTime ?></div>
+                        <div><?= RHtmlHelper::decode($commentItem['root']->content) ?></div>
+
+                        <div style="float: right;">
+                            <?= RHtmlHelper::linkAction('post', 'Reply', 'view',
+                                $commentItem['root']->topicId . '?reply=' . $commentItem['root']->id . '#reply',
+                                array('class' => 'btn btn-xs btn-info')) ?>
+                        </div>
+
+                        <div class="comment-reply-list" style="margin-top: 10px;padding-left: 20px;">
+                            <?php
+                            foreach ($commentItem['reply'] as $reply) {
+                                ?>
+                                <div class="comment-reply-item" style="margin-top: 10px;"><?= RHtmlHelper::linkAction('user', $reply->user->name, 'view', $reply->user->id) ?>
+                                    &nbsp;&nbsp;<?= $reply->createdTime ?></div>
+                                <div><?= RHtmlHelper::decode($reply->content) ?></div>
+                            <?php
+                            }
+                            ?>
+                        </div>
+
+                    </div>
+                    <br/><br/>
+                </div>
+                <hr>
+            <?php
             }
             ?>
-            <br/>
-            <?= RFormHelper::input(array('class' => 'btn btn-sm btn-primary', 'type' => 'submit', 'value' => 'Comment')) ?>
-            <?= RFormHelper::endForm() ?>
+
+
+            <hr/>
+            <div id="reply">
+                <?= RFormHelper::openForm("post/comment/$topic->id", array('id' => 'viewFrom')) ?>
+                <?=
+                RFormHelper::textarea(array(
+                    'name' => 'content',
+                    'class' => 'form-control',
+                    'rows' => '5',
+                    'placeholder' => 'Comment content',
+                ), isset($parent) ? '@' . $parent->user->name . ' ' : '')?>
+                <?php
+                if (isset($parent)) {
+                    echo RFormHelper::hidden('replyTo', ((int)$parent->pid === 0 ? $parent->id : $parent->pid));
+                    echo RFormHelper::hidden('exactReplyTo', $parent->id);
+                }
+                ?>
+                <br/>
+                <?= RFormHelper::input(array('class' => 'btn btn-sm btn-primary', 'type' => 'submit', 'value' => 'Comment')) ?>
+                <?= RFormHelper::endForm() ?>
+            </div>
         </div>
+
     </div>
+
 </div>
