b3e7a4c57200564ed00ac228f93dd3b0d59aa23f
Raysmond <jiankunlei@126.com>
2013-12-08 21:21:22 +0800

    ui optimization

--- app/modules/posts_category/posts_category.view.php
+++ app/modules/posts_category/posts_category.view.php
@@ -5,7 +5,7 @@
         foreach ($categories as $category) {
             ?>
             <li>
-                <a class="btn btn-sx parent-category " href="<?= RHtmlHelper::siteUrl('post/find/' . $category->id) ?>">
+                <a class="btn btn-sx parent-category <?=($category->id==$id)?"active":""?>" href="<?= RHtmlHelper::siteUrl('post/find/' . $category->id) ?>">
                     <?= RHtmlHelper::showImage('files/images/category/' . $category->id . '.png', '', ['style' => 'width:24px;height:24px;']) ?>
                     &nbsp;
                     <?= $category->name ?>
