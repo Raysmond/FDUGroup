679de3e5dce11357c98611e88f1a0d3e0fcafec2
RenchuSong <songrenchu@sina.com>
2013-12-11 00:47:03 +0800

    about

--- app/views/site/about.php
+++ app/views/site/about.php
@@ -2,10 +2,26 @@
  // about view file
  // @author: Raysmond
 ?>
-<div class="jumbotron">
-    <h1>About FDUGroup</h1>
-    <p>
-        Welcome to about page. This is the right web site where you can set up or join groups and share things as
-        you like.
-    </p>
+<?php
+/**
+ * Contact view file.
+ * @author: songrenchu
+ */
+?>
+<div class="panel panel-default">
+    <div  id="developer" class="panel-heading">
+        <h1 class="panel-title">About FDUGroup</h1>
+    </div>
+
+    <div class="panel-body site-hook">
+        <h3>FDUGroup is the right website </h3><h4>Where you can set up or join groups and share things as you like.</h4>
+        <?php
+            for ($i = 1; $i <= 21; ++$i) {
+                ?>
+                <img src="<?=RHtmlHelper::siteUrl('public/images').'/laugh'.$i.'.jpg'?>" style="max-height:100px;min-height:100px;" title="laugh">
+                <?php
+            }
+        ?>
+
+    </div>
 </div>
